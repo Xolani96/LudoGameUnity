dbd270e31413a9039acd0be603e0042ce4b01b6a
X <Nhlapho7@gmail.com>
2020-09-28 23:28:23 +0200

    Unity practise
    Lesson 9 from Youtube videos

---
+++ E09/Assets/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+    
+    public Transform sphereTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereTransform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, 180 *Time.deltaTime, 0);
+    }
+}
