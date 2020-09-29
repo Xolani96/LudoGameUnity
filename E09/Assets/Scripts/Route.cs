f9882938e90fed9fb5ec91162e68bbdc5bfd2ccf
X <Nhlapho7@gmail.com>
2020-09-29 23:26:58 +0200

    Route 1

---
+++ E09/Assets/Scripts/Route.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+    Transform[] childNodes;
+    public List<Transform> childNodeList = new List<Transform>();
+    void Start()
+    {
+        FillNodes();   
+    }
+    void FillNodes(){
+        childNodeList.Clear(); 
+        childNodes = GetComponentsInChildren<Transform>();
+
+        foreach(Transform child in childNodes){
+            if(child != this.transform){
+                childNodeList.Add(child);
+            }
+        }
+    }
+}
